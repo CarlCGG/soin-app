89017931742419d9bf54cc7b8a0dd6ab7b1be05b
CarlCGG <155146997+CarlCGG@users.noreply.github.com>
2026-03-22 22:52:47 +0000

    fix gitignore remove node_modules

---
+++ src/events/events.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { EventsService } from './events.service';
+import { EventsController } from './events.controller';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: 'my_secret_key',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [EventsService],
+  controllers: [EventsController],
+})
+export class EventsModule {}
